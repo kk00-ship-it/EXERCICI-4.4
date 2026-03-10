d4dbb8cb94092c63b2adc442b2d907829053af54
kk00-ship-it <kk00@iesemilidarder.com>
2026-03-10 13:37:40 +0100

    Add files via upload

---
+++ Ejercicio_4-4/JS/ej4-4.js
@@ -0,0 +1,24 @@
+function Calculo(operacion) {
+
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+
+    let result;
+
+        switch(operacion){
+            case 0:
+                result = num1 + num2;
+                break;
+            case 1:
+                result = num1 - num2;
+                break;
+            case 2:
+                result = num1 * num2;
+                break;
+            case 3:
+                result = num1 / num2;
+                break;
+        }
+        document.getElementById("output").innerHTML = result;
+    }
+
